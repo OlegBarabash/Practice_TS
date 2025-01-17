14d8edba886975903208dd754f094e9d334ee9b3
OlegBarabash <olegbarabash7@gmail.com>
2025-01-17 17:19:56 +0200

    End of Lesson 6

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,101 @@
 "use strict";
-// ==== Type Assertion or Type Casting ==========
-// convert to more or less specific
-let a = 'hello';
-let b = a; // less specific
-let c = a; // more specific
-let d = 'world'; // do't work in react
-let e = 'world'; // do't work in react
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b;
-};
-let myVal = addOrConcat(2, 2, 'concat');
-// Be careful! TS sees no problem - but a string is returned
-let nextVal = addOrConcat(2, 2, 'concat');
-// 10 as string //  not ok
-10;
-// The DOM
-const img = document.querySelector('img'); // '!' - nut null
-const myImg = document.getElementById('#img');
-img.src;
+// ====== Classes ==========
+// == Long form ====
+// class Coder {
+//     name: string
+//     music: string
+//     age: number
+//     lang: string
+//     constructor(
+//         name: string,
+//         music: string,
+//         age: number,
+//         lang: string
+//     ) {
+//         this.name = name
+//         this.music = music
+//         this.age = age
+//         this.lang = lang
+//     }
+// }
+// == Short ==
+class Coder {
+    constructor(name, music, age, lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
+// console.log(Dave.age); // it will work But d'nt do that
+// console.log(Dave.lang);  // it will work
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'lofi', 25);
+console.log(Sara.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+////////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log(Peeps.count);
+console.log(Steve.id);
+console.log(Amy.id);
+console.log(John.id);
+/////////////////////////////////////
+// ===== GET SET ======
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'Linkin Park'];
+console.log(MyBands.data);
